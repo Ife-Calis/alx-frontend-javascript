cbb91be4032d57a476df068906542570092c2e5c
Ife-Calis <calistusifechukwu39@gmail.com>
2024-04-26 00:11:56 +0000

    ES6 classes file update

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,17 +1,7 @@
-// Import the ClassRoom class from 0-classroom.js
-const { ClassRoom } = require('./0-classroom');
+import ClassRoom from './0-classroom';
 
-// Implement the initializeRooms function
-function initializeRooms() {
-    // Create an array of ClassRoom objects with the specified sizes
-    const rooms = [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34)
-    ];
-    return rooms;
+export default function initializeRooms() {
+  const sizes = [19, 20, 34];
+
+  return sizes.map((size) => new ClassRoom(size));
 }
-
-// Export the initializeRooms function
-module.exports = { initializeRooms };
-
